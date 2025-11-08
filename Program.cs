8934416198f9f81debf73f7fbfd3d3a2f5307d42
Dawid <dawidkiciski7@gmail.com>
2025-11-08 19:51:56 +0100

    zad1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,36 @@
-﻿namespace Lab1
-{
+﻿using System;
 
+namespace ProgObiekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Powitanie 
+            Console.WriteLine("Obliczanie delty");
+            
+            //podajemy a 
+            Console.Write("Podaj a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            //podajemy b
+            Console.Write("Podaj b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            //podajemy c
+            Console.Write("Podaj c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            //tworzymy obiekt z dannymi od użytkownika
+            Quadratic trojmian = new Quadratic(a, b, c);
+
+            //obliczamy delte
+            double delta = trojmian.ComputerDelta();
+
+            //wyświetlamy wynik gotowych wyróżników 
+            Console.WriteLine($"Wyróżnik (delta) = {delta}");
+
+            // Obliczamy pierwiastki
+            trojmian.Solve();
+        }
+
+    }
 }
---
+++ Quadratic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgObiekt
+{
+    internal class Quadratic
+    {
+        public double A { get; }
+        public double B { get; }
+        public double C { get; }
+
+        public Quadratic(double a, double b, double c)
+        {
+            if (a == 0)
+            {
+                throw new ArgumentException("Coefficient 'a' cannot be zero for a quadratic equation.");
+            }
+
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double ComputerDelta()
+        {
+            double delta = (B * B) - (4 * A * C);
+            return delta;
+        }
+        public void Solve()
+        {
+            double delta = ComputerDelta();
+            if (delta < 0)
+            {
+                Console.WriteLine("No real roots.");
+            }
+            else if (delta == 0)
+            {
+                double root = -B / (2 * A);
+                Console.WriteLine($"One real root: {root}");
+            }
+            else
+            {
+                double root1 = (-B + Math.Sqrt(delta)) / (2 * A);
+                double root2 = (-B - Math.Sqrt(delta)) / (2 * A);
+                Console.WriteLine($"Two real roots: {root1} and {root2}");
+            }
+        }
+    }
+}
